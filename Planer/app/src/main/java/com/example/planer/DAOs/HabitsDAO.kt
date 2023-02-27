f932bb3c3606c24a95e257cc6edffd1de0404fc1
Maciej <mcjstpn@gmail.com>
2023-02-27 15:03:41 +0100

    Rozbudowanie DAO

--- Planer/app/src/main/java/com/example/planer/DAOs/HabitsDAO.kt
+++ Planer/app/src/main/java/com/example/planer/DAOs/HabitsDAO.kt
@@ -1,13 +1,30 @@
 package com.example.planer.DAOs
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
+import androidx.room.*
 import com.example.planer.entities.Habits
 
 @Dao
 interface HabitsDAO
 {
+    //Insertowanie jakiegos habitsa do bazy
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(habits: Habits)
+
+    //Pobieranie listy habitsow z bazy
+    @Query("SELECT * FROM habits")
+    suspend fun getAllHabits(): List<Habits>
+
+    //Pobieranie Habitsa po id
+    @Query("SELECT * FROM habits WHERE id = :habitId")
+    suspend fun getHabitById(habitId: IntArray): List<Habits>
+
+    //Usuwanie po parametrach
+    @Delete
+    suspend fun delete(habits: Habits)
+
+    //Usuwanie wszystkiego
+    @Query("DELETE FROM habits")
+    suspend fun deleteAll()
+
+
 }
