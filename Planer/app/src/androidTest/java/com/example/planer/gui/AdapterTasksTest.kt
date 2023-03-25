847cc7e74ddfee3cd904251ae8aba6e1580bf34b
Kacper Brzezinski <kacperbrzoza@o2.pl>
2023-03-25 07:46:04 +0100

    *Dodanie klasy do testow AdapterTasks *Dalsze prace nad testami HomeFragment

---
+++ Planer/app/src/androidTest/java/com/example/planer/gui/AdapterTasksTest.kt
@@ -0,0 +1,18 @@
+package com.example.planer.gui
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class AdapterTasksTest{
+
+    @Test
+    fun adapterTasksTest(){
+
+
+    }
+}
