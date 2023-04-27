c5ed5e6ba715cf9891111b7a966a68ddebb47260
Kacper Brzezinski <kacperbrzoza@o2.pl>
2023-04-27 13:52:37 +0200

    update pracy na dzis w przypadku odhaczenia zadania

--- Planer/app/src/main/java/com/example/planer/tasks/Subtask.kt
+++
@@ -1,7 +0,0 @@
-package com.example.planer.tasks
-
-//klasa podzadań, które można dodawać do tasków
-open class Subtask(_title: String, _active: Boolean){
-    var title: String = _title
-    var active: Boolean = _active
-}
